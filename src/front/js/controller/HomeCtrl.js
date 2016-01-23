4d2de50a59b02808930efb7437dcf47f14701bd1
Kevin Amiranoff <kamiranoff@gmail.com>
2016-01-23 13:19:21 +0100

    adds get request to api

--- src/front/js/controller/HomeCtrl.js
+++ src/front/js/controller/HomeCtrl.js
@@ -1,1 +1,11 @@
-HomeCtrl.js
+app.controller('HomeCtrl', function($scope,$http) {
+
+  $http.get('api/carnets').
+    success(function(data, status, headers, config) {
+      $scope.posts = data;
+    }).
+    error(function(data, status, headers, config) {
+      // log error
+    });
+
+});

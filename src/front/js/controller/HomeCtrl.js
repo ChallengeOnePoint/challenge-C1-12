82784f895cbd7d70a779a3f29ae68f61f902df6c
Kevin Amiranoff <kamiranoff@gmail.com>
2016-01-23 13:26:24 +0100

    changes scope objects

--- src/front/js/controller/HomeCtrl.js
+++ src/front/js/controller/HomeCtrl.js
@@ -2,7 +2,7 @@ app.controller('HomeCtrl', function($scope,$http) {
 
   $http.get('api/carnets').
     success(function(data, status, headers, config) {
-      $scope.posts = data;
+      $scope.adresses = data;
     }).
     error(function(data, status, headers, config) {
       // log error

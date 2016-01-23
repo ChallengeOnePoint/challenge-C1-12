fa09d049e496d0af99845f5987ffac4b885d526e
Nepuchun <kameron.argis@gmail.com>
2016-01-23 13:38:51 +0100

    ajout ng-repeat

--- src/front/js/controller/HomeCtrl.js
+++ src/front/js/controller/HomeCtrl.js
@@ -3,6 +3,7 @@ app.controller('HomeCtrl', function($scope,$http) {
   $http.get('api/carnets').
     success(function(data, status, headers, config) {
       $scope.adresses = data;
+      console.log($scope.adresses);
     }).
     error(function(data, status, headers, config) {
       // log error
